56a280ac2b9e5c13a06eb7c203dbd3ec2709e282
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-05-05 10:07:51 +0530

    modify for hosting

--- src/test/java/com/ashish/planservice/PlanServiceApplicationTests.java
+++ src/test/java/com/ashish/planservice/PlanServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.ashish.planservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PlanServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class PlanServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
